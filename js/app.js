130c9994723eecd380f1f87771188a9b8ef1fcf7
Aileen Murphy <ai.murf2@gmail.com>
2019-01-30 11:02:47 -0800

    updated js and added index.html

---
+++ js/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Question 1
+
+var starwars = prompt('Does Aileen like Star Wars more than Star Trek?');
+console.log(starwars);
+
+if(starwars==='yes'){
+alert('Yes! But only the first three.')
+} else {
+  alert('No, I\'m not a trekkie.')
+}
+
+// Question 2
+
+var age = prompt('Aileen is closer to 20 than 40?');
+console.log(age);
+
+if (age==='yes'){
+  alert('No, silly, I have 2 kids! I gotta live my life first!')
+  }else{
+  alert('Harsh, man.')
+  }
+
+// Quesiton 3
+
+var veggies = prompt('Is Aileen a vegetarian?');
+console.log(veggies);
+
+if (veggies==='yes'){
+  alert('No ways. Meat is delicious murder. Forgive me')
+}else{
+  alert('You\'re damn right I\'m not!')
+}
+
+// Question 4
+
+var seahawks = prompt('Is Aileen a Seahawks fan?');
+console.log(seahawks);
+
+if (seahawks==='yes'){
+  alert('Technically, yeeeeeeess...')
+}else{
+  alert('Not a Seahawks fan, necessarily, I\'m a Seattle fan. This is my hometown so I always root for the home team.')
+}
+
+// Question 5
+
+var korean = prompt('Is Aileen Korean?');
+console.log(korean);
+
+if(korean==='yes'){
+  alert('Mostly')
+}else{
+  alert('You\'re wrong!')
+}
