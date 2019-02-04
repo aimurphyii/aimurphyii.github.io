144aed75ea51869b49959b6b137067ccc722a461
Aileen Murphy <ai.murf2@gmail.com>
2019-02-03 17:15:30 -0800

    added scorekeeper, updated q 6 and 7, lined up for lab 3, no function wraps on this file

--- js/app03.js
+++ js/app03.js
@@ -17,9 +17,11 @@ var lilstarwars = starwars.toLowerCase();
 // make that logic
 if (lilstarwars === 'yes' || lilstarwars === 'y'){
   alert('May the force be with you!');
+  console.log ('correct');
 }else{
   alert('Wrong. You don\'t know me, boo.');
 }
+
 
 // ***Q2***
 
@@ -32,6 +34,7 @@ var lilage = age.toLowerCase();
 // if~then
 if (lilage === 'yes' || lilage === 'y'){
   alert('God bless the 80s!');
+  console.log ('correct');
 }else{
   alert('Wrong, child of the 80s, right here. RIP Challenger.');
 }
@@ -48,6 +51,7 @@ if (lilveggies === 'yes' || lilveggies === 'y'){
   alert('This one time, in college... I tried. I really tried.');
 }else{
   alert('Tried it twice, and I\'ll only go back kicking and screaming.');
+  console.log ('correct');
 }
 
 // ***Q4***
@@ -60,8 +64,10 @@ var lilseahawks = seahawks.toLowerCase();
 // yes, or else!
 if(lilseahawks === 'yes' || lilseahawks === 'y'){
   alert('MMmmm... well, not exactly.');
+  console.log ('correct');
 }else{
   alert('Not a true Seahawks fan, but I\'m a fan by default. I\'m a Seattle fan. This is my hometown so I always root for the hometeam.');
+  console.log('correct');
 }
 
 //  ***Q5***
@@ -75,12 +81,13 @@ var lilkorean = korean.toLowerCase();
 // so, what is she?
 if (lilkorean === 'yes' || lilkorean === 'y'){
   alert('Mostly~ but, wait! There\'s more...');
+  console.log ('correct');
 }else{
   alert('You\'re not entirely wrong.')
+  console.log('correct');
 }
 
 //  ***Q6***
-
 var married = prompt('How many years have I been married?');
 
 // you get 4 guesses
@@ -90,8 +97,12 @@ if (married > 7){
   var married = prompt ('Not quite that long, try lower...');
 }else if (married < 7){
   var married = prompt ('We beat those odds, try higher...');
-}else{
-  alert('Bingo!');{break;}
+}else if (married === '7'){
+  alert('Bingo!');
+  console.log('correct');
+  {break;}
+}else {
+  var married = prompt('Use a number, please~');
 }
 tries--;
 }
@@ -99,26 +110,25 @@ tries--;
 // ***Q7***
 
 
-// var tries = 6
-// while (tries < 6){}
-
-// var studied = [
-//     'korean',
-//     'japanese',
-//     'french',
-//     'spanish'
-//     ];
-// for (i=6; i>0; i--){
-//     var lang = prompt('What languages has Aileen studied?');
-//     console.log(lang);
-//     if (lang === studied[i]){
-//       var lang = prompt('Yes! And...');
-//     }else{
-//       var lang = prompt('not yet, try another')
-//     }
-// }
-
-
+var places = [
+  'north carolina',
+  'maryland',
+  'florida',
+  'washington',
+  'japan',
+  'georgia',
+  'california',
+  'texas',
+];
+var homes = prompt('I\'ve moved around in my life, can you guess some states or countries I\'ve called home?');
+for (var tries = 6; tries > 0; tries--){
+  if (places.indexOf(homes.toLowerCase()) >= 0){
+    var homes = prompt('Yes! And...');
+    console.log('correct');
+  }else{
+    var homes = prompt('Not yet, try another...')
+  }
+}
 
 // outro
 // What's my name!?
@@ -129,4 +139,5 @@ while (myName.toLowerCase() !== 'aileen'){
 }
 if (myName.toLowerCase() === 'aileen'){
   alert('Yes! Best friends, forever!');
+  console.log ('correct');
 }
--- js/app03b.js
+++ js/app03b.js
@@ -3,12 +3,12 @@
 // get the user's name
 var hiFriend = prompt('Heyo~! Aileen, here. Tell me, stranger, what\'s your name?');
 console.log(hiFriend);
-alert('Welcome, ' + hiFriend + '! Let\'s see where you line up on the stalker/friendship scale...')
 
-var answers = [];
+alert('Welcome, ' + hiFriend + '! Let\'s see where you line up on the stalker/friendship scale...')
 
 // ***Q1***
 
+function Q1(){
 var starwars = prompt('Does Aileen like Star Wars more than Star Trek?');
 console.log(starwars);
 
@@ -18,11 +18,10 @@ var lilstarwars = starwars.toLowerCase();
 // make that logic
 if (lilstarwars === 'yes' || lilstarwars === 'y'){
   alert('May the force be with you!');
-  answers.push('Q1');
 }else{
   alert('Wrong. You don\'t know me, boo.');
 }
-
+}
 
 // ***Q2***
 
@@ -35,7 +34,6 @@ var lilage = age.toLowerCase();
 // if~then
 if (lilage === 'yes' || lilage === 'y'){
   alert('God bless the 80s!');
-  answers.push('Q2');
 }else{
   alert('Wrong, child of the 80s, right here. RIP Challenger.');
 }
@@ -52,7 +50,6 @@ if (lilveggies === 'yes' || lilveggies === 'y'){
   alert('This one time, in college... I tried. I really tried.');
 }else{
   alert('Tried it twice, and I\'ll only go back kicking and screaming.');
-  answers.push('Q3');
 }
 
 // ***Q4***
@@ -65,10 +62,8 @@ var lilseahawks = seahawks.toLowerCase();
 // yes, or else!
 if(lilseahawks === 'yes' || lilseahawks === 'y'){
   alert('MMmmm... well, not exactly.');
-  answers.push('Q4');
 }else{
   alert('Not a true Seahawks fan, but I\'m a fan by default. I\'m a Seattle fan. This is my hometown so I always root for the hometeam.');
-  answers.push('Q4');
 }
 
 //  ***Q5***
@@ -82,13 +77,12 @@ var lilkorean = korean.toLowerCase();
 // so, what is she?
 if (lilkorean === 'yes' || lilkorean === 'y'){
   alert('Mostly~ but, wait! There\'s more...');
-  answers.push('Q5');
 }else{
   alert('You\'re not entirely wrong.')
-  answers.push('Q5');
 }
 
 //  ***Q6***
+
 var married = prompt('How many years have I been married?');
 
 // you get 4 guesses
@@ -98,57 +92,43 @@ if (married > 7){
   var married = prompt ('Not quite that long, try lower...');
 }else if (married < 7){
   var married = prompt ('We beat those odds, try higher...');
-}else if (married === '7'){
-  alert('Bingo!');
-  answers.push('Q6');
-  {break;}
-}else {
-  var married = prompt('Use a number, please~');
+}else{
+  alert('Bingo!');{break;}
 }
 tries--;
 }
 
 // ***Q7***
 
-// places i've lived
-var places = [
-  'north carolina',
-  'maryland',
-  'florida',
-  'washington',
-  'california',
-  'texas',
-];
-var homes = prompt('I\'ve moved around in my life, can you guess some states I\'ve called home? (6 possible answers)');
-// you get 6 tries
-for (var tries = 6; tries > 1; tries--){
-  if (places.indexOf(homes.toLowerCase()) >= 0){
-    var homes = prompt('Yes! And...');
-    answers.push('Q7');
-  }else{
-    var homes = prompt('Not yet, try another...')
-  }
-}
+
+// var tries = 6
+// while (tries < 6){}
+
+// var studied = [
+//     'korean',
+//     'japanese',
+//     'french',
+//     'spanish'
+//     ];
+// for (i=6; i>0; i--){
+//     var lang = prompt('What languages has Aileen studied?');
+//     console.log(lang);
+//     if (lang === studied[i]){
+//       var lang = prompt('Yes! And...');
+//     }else{
+//       var lang = prompt('not yet, try another')
+//     }
+// }
+
+
 
 // outro
 // What's my name!?
 var myName = prompt('What\'s my name?');
 
 while (myName.toLowerCase() !== 'aileen'){
-  var myName = prompt('WRONG! Hint, it\'s like a girl with only 1 leg and begins with an A...');
+  var myName = prompt('WRONG! Hint, it\'s like a girl with only 1 leg...');
 }
 if (myName.toLowerCase() === 'aileen'){
   alert('Yes! Best friends, forever!');
-  answers.push ('bonusQ');
 }
-
-// keeping score
-
-if (answers.length > 10){
-  alert(hiFriend + ', if we aren\'t besties, we need to have a chat. ' + answers.length + ' out of 12 points is stalker-country.');
-}else if (answers.length < 6){
-  alert('I hope you learned something today, ' + hiFriend + '. ' + answers.length + 'out of 12 points is only the beginning of our friendship. We need to hang out more.');
-}else{
-  alert('Well, '+ hiFriend + ', ' + answers.length + ' out of 12 points? Not too shabby, my friend!');
-}
-
